17dee19f8401ac11bc72dd2803b087cc616ff8c4
Mariah (Tuesday) Hernandez <rahrah93@gmail.com>
2022-05-26 12:28:48 -0700

    configuring sessions controller

--- server.js
+++ server.js
@@ -2,7 +2,8 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const session = require('express-session');
 
 // Database Configuration
 mongoose.connect(process.env.DATABASE_URL, {
@@ -15,6 +16,21 @@ const db = mongoose.connection;
 db.on("error", (err) => console.log(err.message + " mongo not running"));
 db.on("connected", () => console.log("mongo connected"));
 db.on("disconnected", () => console.log("mongo disconnected"));
+
+// Middleware
+// Body parser middleware: give us access to req.body
+app.use(express.urlencoded({ extended: true }));
+// Routes / Controllers
+const userController = require("./controllers/users");
+app.use("/users", userController);
+
+app.use(
+    session({
+        secret: process.env.SECRET,
+        resave: false,
+        saveUninitialized: false
+    }));
+
 
 // Listener
 const PORT = process.env.PORT;
